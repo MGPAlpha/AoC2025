c353ff0ddec6258f0a4289a56663299a7e5e5895
Maeve Philipp <mgpalpha1@gmail.com>
2025-12-10 21:27:38 -0500

    solved day 2 part 2

--- day2.py
+++ day2.py
@@ -1,5 +1,18 @@
 input = []
 
+def test_num(n):
+    num_str = str(i)
+    for seg_len in range(1,len(num_str)):
+        if len(num_str) % seg_len != 0: continue
+        part1 = num_str[:seg_len]
+        len_works = True
+        for seg_start in range(seg_len, len(num_str), seg_len):
+            if part1 != num_str[seg_start: seg_start + seg_len]:
+                len_works = False
+                break
+        if len_works: return True
+    return False
+
 with open("day2input.txt", "r") as file:
     input = file.read().split(",")
 
@@ -10,11 +23,7 @@ for ran in input:
     start = int(ran[:index])
     end = int(ran[index+1:])
     for i in range(start, end+1):
-        num_str = str(i)
-        if len(num_str) % 2 == 1: continue
-        part1 = num_str[:len(num_str)//2]
-        part2 = num_str[len(num_str)//2:]
-        if part1 == part2:
+        if test_num(i):
             total += i
     
 print(total)
